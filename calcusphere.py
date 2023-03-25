0f977f61e71de35ad7f75e671e07ab25bc4b62f9
VolatileX1 <128452078+VolatileX1@users.noreply.github.com>
2023-03-25 12:37:25 +0530

    Added functions
    - It now includes scientific functions such as sine, cosine, and tangent.
    - It calculates the value of these functions based on the user's input in degrees.

--- calcusphere.py
+++ calcusphere.py
@@ -18,6 +18,18 @@ def power(num1, num2):
 def sqrt(num):
     return math.sqrt(num)
 
+def sin(degrees):
+    radians = math.radians(degrees)
+    return math.sin(radians)
+
+def cos(degrees):
+    radians = math.radians(degrees)
+    return math.cos(radians)
+
+def tan(degrees):
+    radians = math.radians(degrees)
+    return math.tan(radians)
+
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
@@ -25,9 +37,12 @@ print("3.Multiply")
 print("4.Divide")
 print("5.Power")
 print("6.Sqrt")
+print("7.Sine")
+print("8.Cosine")
+print("9.Tangent")
 
 while True:
-    choice = input("Enter choice(1/2/3/4/5/6): ")
+    choice = input("Enter choice(1/2/3/4/5/6/7/8/9): ")
 
     if choice in ('1', '2', '3', '4', '5', '6'):
         num1 = float(input("Enter first number: "))
@@ -46,6 +61,17 @@ while True:
             print(num1, "^", num2, "=", power(num1, num2))
         elif choice == '6':
             print("sqrt(", num1, ")=", sqrt(num1))
+        else:
+            print("Invalid Input")
+    elif choice in ('7', '8', '9'):
+        degrees = float(input("Enter degrees: "))
+
+        if choice == '7':
+            print("sin(", degrees, ")=", sin(degrees))
+        elif choice == '8':
+            print("cos(", degrees, ")=", cos(degrees))
+        elif choice == '9':
+            print("tan(", degrees, ")=", tan(degrees))
         else:
             print("Invalid Input")
     else:
